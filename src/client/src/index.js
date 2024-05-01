f7e324c0ab402603a92877cf464e81b51d87a4da
Liujia Yu <scyly1@nottingham.ac.uk>
2024-05-01 14:49:55 -0700

    [update] removed .DS_Store and update gitignore

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -8,7 +8,7 @@ import router from './router';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    {/* 2. 路由绑定 */}
+    {/* 2. use router/index.js */}
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
